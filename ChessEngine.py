2b7eb76420dcfde5706d66e4c549dbb5522acf30
Noé Keller <noe.keller@gmx.net>
2021-03-04 09:57:51 +0100

    Hauptklassen erstellt

---
+++ ChessEngine.py
@@ -0,0 +1,4 @@
+"""
+Dieses Skript ist sozusagen das "backend" des Spiels. Es haltet die aktuelle Spielpositionen,
+ist für die Einhaltung der Regeln zuständig, führt ein Movelog etc.
+"""
---
+++ ChessMain.py
@@ -0,0 +1,4 @@
+"""
+Dieses Skrip ist das Frontend, es kümmert sich um das GUI, Interaktion mit dem Nutzer,
+Darstellung des Bretts und des aktuellen Spielstandes etc.
+"""
