f5cdf50bc2718e9c0e4e1c80dfea60b22939da24
vans666 <vanessa.ott@stud.kbw.ch>
2021-03-14 20:36:26 +0100

    "second commit"

--- ChessMain.py
+++ ChessMain.py
@@ -23,17 +23,36 @@ grafik und input
 
 def main():
     p.init()
-    screen = p.display.set_mode(Breite, Höhe)
+    screen = p.display.set_mode((Breite, Höhe))
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = ChessEngine.GameState()
     print(gs.board)
     Bilder_laden()
     running = True
+    QuSelected = ()
+    Spielerklickt = []
     while running:
         for e in p.event.get():
             if e.type == p-QUIT:
                 running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()
+                col = location[0]//Quadrat_Grösse
+                row = location[1]//Quadrat_Grösse
+                if QuSelected == (row, col):
+                    QuSelected = ()
+                    Spielerklickt = []
+                else:
+                    QuSelected = (row, col)
+                    Spielerklickt.append(QuSelected)
+                if len(Spielerklickt) == 2:
+                    move = ChessEngine.Move(Spielerklickt[0], Spielerklickt[1], gs.board)
+                    print(move.getSchachNotation())
+                    gs.makeMove(move)
+                    QuSelected = ()
+                    Spielerklickt = []
+
         drawGameState(screen, gs)
         clock.tick(Max_FPS)
         p.display.flip()
@@ -54,6 +73,11 @@ def drawBoard(screen):
 
 
 def drawFiguren(screen, board):
+    for r in range(Dimension):
+        for c in range(Dimension):
+            Figur = board[r][c]
+            if Figur != "--":
+                screen.blit(Bilder[Figuren],p.Rect(c*Quadrat_Grösse, r*Quadrat_Grösse, Quadrat_Grösse, Quadrat_Grösse))
 
 
 
